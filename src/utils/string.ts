2bfedd01653e879e4f6c98682e5a416f6c26ee89
JBordage <38659460+JustinBordage@users.noreply.github.com>
2024-01-05 16:21:44 -0400

    Implemented utility functions for detecting empty & blank strings
    - Added "isEmpty"
    - Added "isBlank"
    - Added "isNotBlank"

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -2,3 +2,33 @@
 export function reverse(value: string): string {
 	return Array.from(value).reverse().join("");
 }
+
+/** Checks if a string is empty
+ *
+ * @rule undefined -> true
+ * @rule null -> true */
+export function isEmpty(
+	strVal: string | null | undefined,
+): strVal is null | undefined | "" {
+	return (strVal ?? "").length === 0;
+}
+
+/** Checks if a string is blank (whitespace only) or empty
+ *
+ * @rule undefined -> true
+ * @rule null -> true */
+export function isBlank(
+	strVal: string | null | undefined,
+): strVal is null | undefined {
+	return isEmpty(strVal?.trim());
+}
+
+/** Checks if a string is NOT blank (whitespace only) and NOT empty
+ *
+ * @rule undefined -> false
+ * @rule null -> false */
+export function isNotBlank(
+	strVal: string | null | undefined,
+): strVal is string {
+	return !isBlank(strVal);
+}
