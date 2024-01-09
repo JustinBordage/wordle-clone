2eca223f99c1429e85ed81db8444fb933aed2354
JBordage <38659460+JustinBordage@users.noreply.github.com>
2024-01-09 13:25:09 -0400

    Added implementation explanation to the "getWordsList" http request

--- src/adapters/wordle.ts
+++ src/adapters/wordle.ts
@@ -1,3 +1,8 @@
+/** Fetches a list of valid words from the backend
+ *  based on the length of the solution word.
+ *
+ *  @remark I'm emulating a backend by placing
+ *   the files within the "public" directory. */
 export async function getWordsList(wordLength: number): Promise<string[]> {
 	const resp = await fetch(`/api/words/${wordLength}.json`, {
 		method: "GET",
@@ -5,3 +10,8 @@ export async function getWordsList(wordLength: number): Promise<string[]> {
 
 	return resp.ok ? await resp.json() : [];
 }
+
+/** @disclaimer While it would probably be better to just import
+ *   the word lists, I've chosen to emulate a http request to show
+ *   how I might implement such a request in a project without a
+ *   npm package like axios. */
